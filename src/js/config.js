43652820040b0de718f8b481cdbda578d21f0dfd
Aman Chaudhary <agcy7320@gmail.com>
2020-03-16 18:50:50 +0530

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,2 +1,2 @@
-export const key = '7a2c8d3b14f04e373c3429320b5a6762';
+export const key = 'd545cdfa3cmshf69afb54c6276fcp13c538jsn160da9030d07';
 export const proxy = 'https://cors-anywhere.herokuapp.com/';
